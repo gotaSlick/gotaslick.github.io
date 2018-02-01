f12388fecf39fa8807f22e3002464072d94633a9
gotaSlick <30895814+gotaSlick@users.noreply.github.com>
2018-02-01 14:36:19 +0100

    wip

---
+++ sourcecode/publish.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+ 
+# mkdir -p sourcecode
+# mv ./* ./sourcecode 
+# mv ./docs/* ./
+cd sourcecode
+# add commit recent changes
+git add .
+git commit -m 'wip'
+git push origin master 
+
