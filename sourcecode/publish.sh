a86e0d6b196fe31fe79cdd9fead9581e900309f2
gotaSlick <30895814+gotaSlick@users.noreply.github.com>
2018-02-07 22:24:04 +0100

    wip

--- sourcecode/publish.sh
+++ sourcecode/publish.sh
@@ -1,15 +1,16 @@
 #! /bin/bash
  
 # mkdir -p sourcecode
-#mv ./* ./sourcecode 
+# mv ./* ./sourcecode 
 # mv ./docs/* ./
-#cd sourcecode
+# cd sourcecode
 
 # move all folders and files from build to top level
-# cp -rf ./build/. ../
+cp -rf ./build/. ../
+cd ..
 # add commit recent changes
-# git add .
-# git commit -m 'winp'
-# git push origin master 
+git add .
+git commit -m 'wip'
+git push origin master 
 
 
